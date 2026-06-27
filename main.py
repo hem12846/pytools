4f91f32c5997a49fb9d3ddad81890928b4ea3d48
hem12846 <minehut1768@gmail.com>
2026-06-27 19:38:57 +0100

    idk

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 
 from random import randint
 from datetime import datetime
+from datetime import time
 
 print("What would you like to do? ")
 choice = input(" 1 random number gen\n 2 current date\n 3 calculator ")
+#1]  RNG
 if choice == '1':
      num1 = int(input("type the first number: "))
      num2 = int(input("type the second number: "))
      result = randint(num1,num2)
      print("your number is: ", result)
+#2]  Current date
+elif choice == '2':
+     print(f'The current date/time is: {datetime.now()}')
+#3]  calculator
